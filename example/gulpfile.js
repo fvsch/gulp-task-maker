48ded12b1e03b4c6fb387d9b6db47452f96cfd30
Florens Verschelde <florens@fvsch.com>
2017-04-18 13:47:05 +0200

    Tweak gitignore, package.json

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,13 +1,13 @@
 /**
- * Simple test with a task that concatenates and minifies CSS and JS
+ * Simple test with tasks that concatenate and minify CSS and JS
  *
  * For example, the mincss task lives in the 'tasks' directory,
  * and consists of two files:
  * - mincss.js, the main function receiving the config;
- * - mincss.json, which lists the task's dependencies and will be read if one
- *   seems to be missing.
+ * - mincss.json, which lists the task's dependencies and will be
+ *   read if one seems to be missing.
  *
- * For more examples of tasks that work with gulp-task-maker,
+ * For more examples of tasks using gulp-task-maker,
  * see https://github.com/gradientz/assets-builder
  */
 
